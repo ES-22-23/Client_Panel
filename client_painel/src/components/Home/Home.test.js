ef7016fc069d91e44f32e000ee0290b3b1dd7f48
pfig420 <palexandre09@ua.pt>
2022-11-26 02:01:35 +0000

    Revert "Home Test Fix"
    This reverts commit 20de47cae788006400e0fff650050841694dc05b.

--- client_painel/src/components/Home/Home.test.js
+++ client_painel/src/components/Home/Home.test.js
@@ -21,8 +21,11 @@ describe('<Home />', () => {
     button = screen.getByTestId("PropertiesButton");
     expect(button).toBeInTheDocument();
 
-    //button = screen.getByTestId("AccountButton");
-    //expect(button).toBeInTheDocument();
+    button = screen.getByTestId("IntrusionsButton");
+    expect(button).toBeInTheDocument();
+
+    button = screen.getByTestId("AccountButton");
+    expect(button).toBeInTheDocument();
   });
 
 });

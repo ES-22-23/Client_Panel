59d97185d212062302fbebaea9ee985bd8dc1bd2
pfig420 <palexandre09@ua.pt>
2022-11-27 19:58:26 +0000

    Remove dev text

--- src/components/Authentication/Authentication.js
+++ src/components/Authentication/Authentication.js
@@ -17,11 +17,7 @@ const Authentication = () => {
                     <Card className="p-5 text-white shadow" style={{border: "none", borderRadius: "20px", backgroundColor: "rgba(0,0,0,0.60)", textAlign: "start"}}>
                         <h2>SecCom</h2>
                         <h5>Smart Security</h5>
-                        <h6 className="mt-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse mollis ligula non magna
-                            tristique, congue aliquam velit fringilla. Nullam orci enim, tincidunt quis euismod eu, viverra
-                            eget erat. Suspendisse potenti. Aenean volutpat justo et lacus egestas ultrices. In hac
-                            habitasse platea dictumst. Nam porta leo quis est rhoncus, eget fermentum leo varius. Aenean
-                            eleifend vestibulum quam, sed aliquam libero volutpat nec.
+                        <h6 className="mt-4">
                         </h6>
                     </Card>
                     <Button variant="danger" className="px-3 w-50 mt-5" onClick={() => keycloak.login()}>Login</Button>
